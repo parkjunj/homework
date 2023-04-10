95ad4e3b36c053c99b6f45519b56723735d1e10b
parkjunj <130322926+parkjunj@users.noreply.github.com>
2023-04-10 12:37:22 +0900

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,31 @@
+package example;
+
+public class Three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean power= false;
+		char answer;
+		answer=(power==true) ? 'y' : 'n';
+		
+		System.out.println(answer);
+		
+		
+	
+		
+		
+		
+		int x=9;
+		String a ="짝수입니다";
+		String b ="홀수입니다";
+		String result=( x % 2==0) ? a : b;
+		System.out.println(result); 
+		
+		int z=-8;
+		int result2=(z<0) ? z*-1 : z;
+		System.out.println(result2);
+		/*절대값 구하는 문*/
+	}
+	
+
+}
